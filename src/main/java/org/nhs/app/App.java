ffed7575bc676c5a64be6674cb5faa3e5e447972
BenjiXavier <2031225@apps.nsd.org>
2024-10-04 18:59:01 +0000

    done

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,67 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App
+{
+   public static void main(String[] args)
+   {
+      /*for (int i = 1; i <= 5; i++)
+      {
+        for (int j = 1; j <= 4; j++)
+        {
+            System.out.print(i);
+        }
+        System.out.println();
+      }*/
 
-package org.nhs.app;
+      //Practice 1
+      for (int i = 1; i <= 5; i++)
+      {
+        for (int j = 1; j <= i; j++)
+        {
+            System.out.print(i);
+        }
+        System.out.println();
+      }
 
-// Your code goes here
+      //Practice 2 
+      for (int i = 1; i <= 5; i++)
+      {
+        for (int j = 1; j <= 5-i; j++)
+        {
+            System.out.print(".");
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            System.out.print(i);
+        }
+        System.out.println();
+      }
+
+     //Practice 3
+      for (int i = 1; i <= 5; i++)
+      {
+        for (int j = 1; j <= 5-i; j++)
+        {
+            System.out.print(".");
+        }
+        System.out.print(i);
+        for (int j = 1; j <= i-1; j++)
+        {
+            System.out.print(".");
+        }
+        System.out.println();
+      }
+
+      //Challenge
+        for (int i = 0; i<= 9; i++)
+        { 
+            for (int j = 1; j <= i; j++)
+            {
+                for (int k = 0; k <= i; k++)
+                {
+                    
+                }
+                System.out.print(""+i+i);
+            }
+            System.out.println();
+        }
+   }
+}
